472d12e3cdea12a16e490997bbb13f5f00136852
= <=>
2022-09-03 20:12:08 -0500

    V1.7 del Juego del Ahorcado con JS

--- js/game.js
+++ js/game.js
@@ -1,20 +1,18 @@
-document
-  .querySelector(".btn-nuevo-juego")
-  .addEventListener("click", function (e) {
+document.querySelector(".btn-nuevo-juego").addEventListener("click", function (e) {
     window.location.href = "./game.html";
-  });
+});
 
 document.querySelector(".btn-cancelar").addEventListener("click", function (e) {
-  window.location.href = "./";
+    window.location.href = "./";
 });
 
 document.querySelector(".bi-github").addEventListener("click", function (e) {
-  window.open("https://github.com/JakubSkraly", "_blank");
+    window.open("https://github.com/JakubSkraly", "_blank");
 });
 
 document.querySelector(".bi-linkedin").addEventListener("click", function (e) {
-  window.open(
-    "https://www.linkedin.com/in/jose-leonel-inga-quintana/",
-    "_blank"
-  );
+    window.open(
+        "https://www.linkedin.com/in/jose-leonel-inga-quintana/",
+        "_blank"
+    );
 });
